c5482d8c681ab836dc950b5060bd4f4219701a9c
Syed Feroz <Syed Feroz@Syed_Feroz.mshome.net>
2024-01-09 12:44:23 +0530

    TestEng1

---
+++ Jenkinsdemo/src/test/java/TestCases/TC01Test.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC01Test {
+	@Test
+	public void m1()
+	{
+		Reporter.log("m1 is executed",true);
+	}
+
+}
---
+++ Jenkinsdemo/src/test/java/TestCases/TC02Test.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC02Test {
+	@Test
+	public void m2()
+	{
+		Reporter.log("m2 is executed",true);
+	}
+
+}
